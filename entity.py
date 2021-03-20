d6a86fb9c3956dc3d74a7f8c8ec41b50de2175aa
Frederic Crete <fredericrete13@gmail.com>
2021-03-20 14:53:14 -0400

    Part 4 WIP

--- entity.py
+++ entity.py
@@ -2,13 +2,28 @@
 Generic class to represent mostly everything in the game
 """
 
-from typing import Tuple
+from __future__ import annotations
+
+from numpy import copy
+from typing import Tuple, TypeVar, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from game_map import GameMap
+
+T = TypeVar("T", bound="Entity")
 
 class Entity:
     """
     A generic object to represent players, enemies, items, etc.
     """
-    def __init__(self, x: int, y: int, char: str, color: Tuple[int, int, int]):
+    def __init__(self,
+                 x: int = 0,
+                 y: int = 0,
+                 char: str = "?",
+                 color: Tuple[int, int, int] = (255,255,255),
+                 name: str = "<Unnamed>",
+                 blocks_movement: bool = False,
+    ):
         """
         Initializer for an entity
         :param x: horizontal position
@@ -20,6 +35,18 @@ class Entity:
         self.y = y
         self.char = char
         self.color = color
+        self.name = name
+        self.blocks_movement = blocks_movement
+
+
+    def spawn(self: T, gamemap: GameMap, x: int, y: int) -> T:
+        """Spawn a copy of this instance at the given location"""
+        clone = copy.deepcopy(self)
+        clone.x = x
+        clone.y = y
+        gamemap.entities.add(clone)
+        return clone
+
 
     def move(self, dx: int, dy: int) -> None:
         """
--- main.py
+++ main.py
@@ -5,9 +5,9 @@ Roguelike project following tutorial
 By FredTheHunterProgrammer
 """
 import tcod
-
+from numpy import copy
 from engine import Engine
-from entity import Entity
+import entity_factories
 from input_handlers import EventHandler
 from procgen import generate_dungeon
 
@@ -22,31 +22,33 @@ def main():
     room_min_size = 6
     max_rooms = 30
 
+    max_monsters_per_room = 2
+
     tileset = tcod.tileset.load_tilesheet(
         "defaultImageFile.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
     event_handler = EventHandler()
 
-    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
-    npc = Entity(int(screen_width / 2-5), int(screen_height / 2), "@", (255, 255, 0))
-    entities = {npc, player}
+    player = copy.deepcopy(entity_factories.player)
+
     game_map = generate_dungeon(
         max_rooms=max_rooms,
         room_min_size=room_min_size,
         room_max_size=room_max_size,
         map_width=map_width,
         map_height=map_height,
+        max_monsters_per_room=max_monsters_per_room,
         player=player
     )
-    engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
+    engine = Engine(event_handler=event_handler, game_map=game_map, player=player)
 
     with tcod.context.new_terminal(
         screen_width,
         screen_height,
         tileset=tileset,
         vsync=True,
-        title="Roguelike Tutorial"
+        title="My First Roguelike :)"
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
